7a0fa32da5e68f18a3aa129645ba4abd1c7d59cc
Renato Carvalho <renato@resolvendobug.com.br>
2023-03-13 22:44:28 -0300

    Add entity framework

--- WebMvc/Models/Department.cs
+++ WebMvc/Models/Department.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace WebMvc.Models
 {
-    public class Department
+    public class Department 
     {
         public int Id { get; set; }
         public string? Name { get; set; }
